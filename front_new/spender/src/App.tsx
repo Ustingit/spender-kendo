0b3dfbe665255b653df035723472b320d83f5f68
yuryi.ustinovich <ustinhtc@gmail.com>
2022-07-05 21:23:30 +0300

    move navbar to component

--- front_new/spender/src/App.tsx
+++ front_new/spender/src/App.tsx
@@ -7,47 +7,13 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import TopNavbar from './components/navbars/TopNavbar'
 
 function App() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Where are the money, Lebovsky ?</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Types</Nav.Link>
-            <NavDropdown title="Reports" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Month-To-Month</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Aggregated-Year
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Full-time
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Finansial advice
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container fluid >
+      <TopNavbar />
+   </Container>
   );
 }
 
