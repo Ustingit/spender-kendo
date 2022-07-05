95c4a84443aea47115934fb194688f4422c5fcbb
yuryi.ustinovich <ustinhtc@gmail.com>
2022-07-05 21:13:57 +0300

    add bootstrap and its initial navbar

--- front_new/spender/src/App.tsx
+++ front_new/spender/src/App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Where are the money, Lebovsky ?</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Types</Nav.Link>
+            <NavDropdown title="Reports" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Month-To-Month</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Aggregated-Year
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Full-time
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Finansial advice
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
---
+++ front_new/spender/src/business/SpentInterface.ts
@@ -0,0 +1,14 @@
+export default interface ISpent {
+    id:'number',
+    amount: 'number',
+    user: 'number',
+    type: 'number',
+    typeName: 'string',
+    subType: 'number',
+    subTypeName: 'string',
+    date: 'Date',
+    isChanged: 'boolean',
+    isFrequent: 'boolean',
+	comment: 'string',
+	currencySign: 'string'
+}
